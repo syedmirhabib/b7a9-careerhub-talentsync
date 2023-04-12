b93fce151683316d5293e18cac6d21e3060a4fc7
Syed Mir Habib <mirakik.habib@gmail.com>
2023-04-12 20:16:58 +0600

    error is updated

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { FaceFrownIcon } from '@heroicons/react/24/solid'
+import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
 
 const Error = () => {
+  const { error, status } = useRouteError()
   return (
-    <div>
-      <h2>sorry!</h2>
-    </div>
-  );
-};
+    <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
+      <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+        <FaceFrownIcon className='w-40 h-40 text-indigo-500' />
+        <div className='max-w-md text-center'>
+          <h2 className='mb-8 font-extrabold text-9xl text-indigo-500'>
+            <span className='sr-only'>Error</span>
+            {status || 404}
+          </h2>
+          <p className='text-2xl font-semibold md:text-3xl text-indigo-800 mb-8'>
+            {error?.message}
+          </p>
+          <Link to='/' className='applied-job-btn'>
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-export default Error;
+export default Error
