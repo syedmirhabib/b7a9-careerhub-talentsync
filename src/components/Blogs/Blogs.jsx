18b679c9c16e9dcf7a7d18ec9a23174a4740c6cf
Syed Mir Habib <mirakik.habib@gmail.com>
2023-04-12 20:17:09 +0600

    blog is updated

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import { useLoaderData } from 'react-router-dom';
+import Question from '../Question/Question';
 
 const Blogs = () => {
+  const questions = useLoaderData();
+  console.log(questions);
   return (
-    <div>
-      <h2>blogs</h2>
+    <div className=''>
+      <Banner>Blogs</Banner>
+      <div className='category-container flex flex-col gap-3'>
+        {
+          questions.map(singleQuestion => <Question
+          key={singleQuestion.id}
+          singleQuestion={singleQuestion}
+          ></Question>)
+        }
+      </div>
     </div>
   );
 };
