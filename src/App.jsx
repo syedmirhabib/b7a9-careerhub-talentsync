4bb23aaeff300e3aeb34ab0b2aaac0da6d0595a1
Syed Mir Habib <mirakik.habib@gmail.com>
2023-04-12 19:49:47 +0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <h1 className="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
+
+export default App
